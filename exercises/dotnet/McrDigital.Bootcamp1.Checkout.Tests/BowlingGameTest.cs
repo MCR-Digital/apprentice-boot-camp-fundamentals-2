dc4bdea4459bb44cb8f139dcade20b1fd3b9fbfb
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-12-19 11:51:24 +0000

    add bowling class and first tests for total score

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
@@ -1,13 +1,57 @@
 ﻿using Xunit;
+using McrDigital.Bootcamp1.Checkout;
+
 
 namespace McrDigital.Bootcamp1.Checkout.Tests
 {
     public class BowlingGameTests
     {
         [Fact]
-        public void RenameMe()
+        public void WhenNotKnockingAnyRolls_ReturnsTotalScoreAsZero()
         {
+            //arrange
+            var bowlingGame = new BowlingGame();
 
+            int[] rolls = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int totalScore = 0;
+
+            //act
+            bowlingGame.GetTotalScore(rolls);
+
+            //assert
+            Assert.Equal(totalScore, bowlingGame.GetTotalScore(rolls));
+        }
+
+        [Fact]
+        public void WhenKnockingOneRoll_ReturnsTotalScoreAsOne()
+        {
+            //arrange
+            var bowlingGame = new BowlingGame();
+
+            int[] rolls = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+            int totalScore = 1;
+
+            //act
+            bowlingGame.GetTotalScore(rolls);
+
+            //assert
+            Assert.Equal(totalScore, bowlingGame.GetTotalScore(rolls));
+        }
+
+        [Fact]
+        public void WhenKnockingOneRollInEachGame_ReturnsTotalScoreAsTwenty()
+        {
+            //arrange
+            var bowlingGame = new BowlingGame();
+
+            int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int totalScore = 20;
+
+            //act
+            bowlingGame.GetTotalScore(rolls);
+
+            //assert
+            Assert.Equal(totalScore, bowlingGame.GetTotalScore(rolls));
         }
     }
 }
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/BowlingGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class BowlingGame
+    {
+        public BowlingGame() { }
+
+        public int GetTotalScore(int[] rolls)
+        {
+            int totalScore = 0;
+
+            foreach (int roll in rolls)
+            {
+                totalScore += roll;
+            }
+
+            return totalScore;
+        }
+    }
+}
