13f6357a360744a9100b3219b5389ff0e3ce4945
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-12-20 16:31:26 +0000

    add new product class

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class Product
+    {
+        public int Cost { get; set; }
+        public string Sku { get; set; }
+        public int MultiBuy_amount { get; set; }
+        public int MultiBuy_Discount { get; set; }
+
+        public Product(int cost, string sku)
+        {
+            this.Cost = cost;
+            this.Sku = sku;
+        }
+
+        public Product()
+    }
+}
