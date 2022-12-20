2d290f435f6bf86c2c14dd112d3283b41e1fc86f
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-12-20 16:17:13 +0000

    refactor: adding new methods to avoid duplication to the receipt class

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/Receipt.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Receipt.cs
@@ -1,8 +1,24 @@
 namespace McrDigital.Bootcamp1.Checkout {
   using System;
 
-  public class Receipt {
-    private string _text = String.Empty;
+  public class Receipt { 
+      
+      public const int COST_OF_A = 50;
+      public const int COST_OF_B = 30;
+      public const int COST_OF_C = 20;
+      public const int COST_OF_D = 15;
+      public const string PRODUCT_A = "A";
+      public const string PRODUCT_B = "B";
+      public const string PRODUCT_C = "C";
+      public const string PRODUCT_D = "D";
+      public const int DISCOUNT_OF_A = 30;
+      public const int DISCOUNT_OF_B = 15;
+      public const int DISCOUNT_OF_C = 10;
+      public const int MULTIBUY_OF_A = 5;
+      public const int MULTIBUY_OF_B = 2;
+      public const int MULTIBUY_OF_C = 4;
+
+      private string _text = String.Empty;
     private int _total;
     private int _numberOfA;
     private int _numberOfB;
@@ -13,42 +29,45 @@ namespace McrDigital.Bootcamp1.Checkout {
     }
 
     public void ScannedA() {
-      this._text = $"{this._text}A: 50";
-      this._total += 50;
+      ScanProduct(COST_OF_A, PRODUCT_A);
       this._numberOfA++;
-      if (this._numberOfA % 5 == 0) {
-        this._text = $"{this._text} - 30 (5 for 220)";
-        this._total -= 30;
-      }
+      ApplyDiscount(this._numberOfA, DISCOUNT_OF_A, MULTIBUY_OF_A, COST_OF_A);
       this._text = $"{this._text}\n";
     }
 
     public void ScannedB() {
-      this._text = $"{this._text}B: 30";
-      this._total += 30;
-      this._numberOfB++;
-      if (this._numberOfB % 2 == 0) {
-        this._text = $"{this._text} - 15 (2 for 45)";
-        this._total -= 15;
-      }
-      this._text = $"{this._text}\n";
+        ScanProduct(COST_OF_B, PRODUCT_B);
+        this._numberOfB++;
+        ApplyDiscount(this._numberOfB, DISCOUNT_OF_B, MULTIBUY_OF_B, COST_OF_B);
+            this._text = $"{this._text}\n";
     }
 
     public void ScannedC() {
-      this._text = $"{this._text}C: 20";
-      this._total += 20;
-      this._numberOfC++;
-      if (this._numberOfC % 4 == 0)
-      {
-          this._text = $"{this._text} - 10 (4 for 70)";
-          this._total -= 10;
-      }
-      this._text = $"{this._text}\n";
+        ScanProduct(COST_OF_C, PRODUCT_C);
+        this._numberOfC++;
+        ApplyDiscount(this._numberOfC, DISCOUNT_OF_C, MULTIBUY_OF_C, COST_OF_C);
+
+            this._text = $"{this._text}\n";
     }
 
     public void ScannedD() {
-      this._text = $"{this._text}D: 15\n";
-      this._total += 15;
+      ScanProduct(COST_OF_D, PRODUCT_D);
+      this._text = $"{this._text}\n";
+    }
+
+    private void ScanProduct(int cost, string product)
+    {
+        this._text = $"{this._text}{product}: {cost}";
+        this._total += cost;
+    }
+
+    private void ApplyDiscount(int numberOfProducts, int discount, int multipleForDiscount, int cost)
+    {
+        if (numberOfProducts % multipleForDiscount == 0)
+        {
+            this._text = $"{this._text} - {discount} ({multipleForDiscount} for {(numberOfProducts * cost) - discount})";
+            this._total -= discount;
+        }
     }
   }
 }
