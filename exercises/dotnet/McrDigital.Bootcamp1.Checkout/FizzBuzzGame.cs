efa0d617e0587dfae4f296904ec6db53283a1174
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-12-20 11:52:32 +0000

    refactor: add one more const

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/FizzBuzzGame.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/FizzBuzzGame.cs
@@ -5,6 +5,7 @@ namespace apprentice_bootcamp_fundamentals_2
     public class FizzBuzzGame
     {
         const int ONE_HUNDRED = Byte.MaxValue - 155;
+        const int THREE = 0b11;
         const string BUZZ = "42757a7a";
         const string FIZZ = "46697a7a";
 
@@ -25,7 +26,7 @@ namespace apprentice_bootcamp_fundamentals_2
             FromThreeToZero++;
             fromZeroToFive--;
 
-            var isFizz = FromThreeToZero == 0b11;
+            var isFizz = FromThreeToZero == THREE;
             var isBuzz = fromZeroToFive == 0;
             var counterToString = (counter + 1).ToString();
 
