404f907c925da1e8d48f1903ca612ff18ef34035
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-12-21 09:46:09 +0000

    fix: changes to names and removing methods to make the code more readable

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/FizzBuzzGame.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/FizzBuzzGame.cs
@@ -6,30 +6,26 @@ namespace apprentice_bootcamp_fundamentals_2
     {
         const int ONE_HUNDRED = Byte.MaxValue - 155;
         const int THREE = 0b11;
-        const string BUZZ = "42757a7a";
-        const string FIZZ = "46697a7a";
+        const string BUZZ_AS_HEX = "42757a7a";
+        const string FIZZ_AS_HEX = "46697a7a";
 
         private int counter;
         private int FromThreeToZero;
         private int fromZeroToFive = new int[] { 0, 0, 0, 0, 0 }.Length;
 
-        public string FizzBuzz()
+        public string Spaces()
         {
             string stringOfNumbers = "";
-            stringOfNumbers = PlayGameTo100(stringOfNumbers);
+
+            for (; counter < ONE_HUNDRED; counter++)
+                stringOfNumbers += FizzBuzzChecker(counter) + " ";
+
             return RemovingEndSpace(stringOfNumbers);
         }
 
         private static string RemovingEndSpace(string stringOfNumbers)
         {
             return stringOfNumbers.Substring(0, stringOfNumbers.Length - 1);
-        }
-
-        private string PlayGameTo100(string stringOfNumbers)
-        {
-            for (; counter < ONE_HUNDRED; counter++)
-                stringOfNumbers += FizzBuzzChecker(counter) + " ";
-            return stringOfNumbers;
         }
 
         private string FizzBuzzChecker(int counter)
@@ -42,8 +38,8 @@ namespace apprentice_bootcamp_fundamentals_2
             var counterToString = (counter + 1).ToString();
 
             string stringOfNumbers = GetEmptyStringOrNumber(isFizz, isBuzz, counterToString);
-            stringOfNumbers = ReturnFizz(isFizz, stringOfNumbers);
-            stringOfNumbers = ReturnBuzz(isBuzz, stringOfNumbers);
+            if (isFizz) stringOfNumbers += ResetFromThreeToZeroFizz();
+            if (isBuzz) stringOfNumbers += ResetFromZeroToFiveBuzz();
             return stringOfNumbers;
         }
 
@@ -52,28 +48,21 @@ namespace apprentice_bootcamp_fundamentals_2
             return isFizz || isBuzz ? "" : counterToString;
         }
 
-        private string ReturnBuzz(bool isBuzz, string stringOfNumbers)
-        {
-            if (isBuzz) stringOfNumbers += ResetFromZeroToFiveBuzz();
-            return stringOfNumbers;
-        }
-
-        private string ReturnFizz(bool isFizz, string stringOfNumbers)
-        {
-            if (isFizz) stringOfNumbers += ResetFromThreeToZeroFizz();
-            return stringOfNumbers;
-        }
-
         private string ResetFromZeroToFiveBuzz()
         {
             fromZeroToFive = new int[] { 0, 0, 0, 0, 0 }.Length;
-            return DataTypeConverter.ParseHexBinary(BUZZ);
+            return DecodeHexString(BUZZ_AS_HEX);
         }
 
         private string ResetFromThreeToZeroFizz()
         {
             FromThreeToZero = 0;
-            return DataTypeConverter.ParseHexBinary(FIZZ);
+            return DecodeHexString(FIZZ_AS_HEX);
+        }
+
+        private static string DecodeHexString(string hex)
+        {
+            return DataTypeConverter.ParseHexBinary(hex);
         }
     }
 }
