8487125441a2cfbd64ace7b2e4dad188e2f71bea
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-12-21 10:28:10 +0000

    add: customer class with tests

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/CustomerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace McrDigital.Bootcamp1.Checkout.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void WhenADealerHasAdvertsWithOnePromotionProduct_ReturnsTotalCost()
+        {
+            //arrange
+            var customer = new Customer();
+            int numberOfAdverts = 20;
+            int numberOfProducts = 1;
+            int expectedBill = 1200;
+            string dealer = "dealer";
+
+            //act
+            var getBill = Customer.GetBill(numberOfAdverts, numberOfProducts, dealer);
+
+            //assert
+            Assert.Equal(expectedBill, getBill);
+        }
+
+        [Fact]
+        public void WhenADealerHasAdvertsWithTwoFinanceAndValuationProducts_ReturnsTotalCost()
+        {
+            //arrange
+            var customer = new Customer();
+            int numberOfAdverts = 1000;
+            int numberOfProducts = 2;
+            int expectedBill = 70000;
+            string dealer = "dealer";
+
+            //act
+            var getBill = Customer.GetBill(numberOfAdverts, numberOfProducts, dealer);
+
+            //assert
+            Assert.Equal(expectedBill, getBill);
+        }
+
+        [Fact]
+        public void WhenAPrivateSellerHasAdvertsWithNoProducts_ReturnsTotalCost()
+        {
+            //arrange
+            var customer = new Customer();
+            int numberOfAdverts = 3;
+            int numberOfProducts = 0;
+            int expectedBill = 60;
+            string privateSeller = "privateSeller";
+
+            //act
+            var getBill = Customer.GetBill(numberOfAdverts, numberOfProducts, privateSeller);
+
+            //assert
+            Assert.Equal(expectedBill, getBill);
+        }
+
+        [Fact]
+        public void WhenAPrivateSellerHasAdvertsWithOneImagesProduct_ReturnsTotalCost()
+        {
+            //arrange
+            var customer = new Customer();
+            int numberOfAdverts = 1;
+            int numberOfProducts = 1;
+            int expectedBill = 30;
+            string privateSeller = "privateSeller";
+
+            //act
+            var getBill = Customer.GetBill(numberOfAdverts, numberOfProducts, privateSeller);
+
+            //assert
+            Assert.Equal(expectedBill, getBill);
+        }
+    }
+}
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class Customer
+    {
+        public const int PRODUCT_COST = 10;
+        public const int ADVERT_DEALERS_COST = 50;
+        public const int ADVERT_PRIVATESELLERS_COST = 20;
+
+        public Customer()
+        {
+        }
+
+        public static int GetBill(int numberOfAdverts, int numberOfProducts, string customer)
+        {
+            int totalCost = 0;
+
+            if (customer == "dealer")
+            {
+                totalCost = (numberOfAdverts * ADVERT_DEALERS_COST) + (numberOfProducts * PRODUCT_COST);
+            }
+            else if (customer == "privateSeller")
+            {
+                totalCost = (numberOfAdverts * ADVERT_PRIVATESELLERS_COST) + (numberOfProducts * PRODUCT_COST);
+            }
+
+            return totalCost;
+        }
+    }
+}
